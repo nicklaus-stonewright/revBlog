aa7179f2b777e1ce23695a0f1282ff60ff147045
Owsama Hajjar <hajjar.owsama@outlook.com>
2022-05-29 12:03:44 +0100

    blogposts element created

--- src/collection/BlogPosts.jsx
+++ src/collection/BlogPosts.jsx
@@ -1,12 +1,51 @@
 import BlogPost from "../components/BlogPost";
 
 const BlogPosts = () => {
+  const blogPostsCollection = [
+    {
+      topic: "performance review",
+      title: "review was focused on finding mistakes",
+      message: "they kept trying to find mistakes in everything I did",
+      owner: "Annie",
+      avatar: "../assets/images/owsama-hat.jpg",
+      status: "active",
+      timestamp: "2022-02-11T07:36:17+0000",
+      company: "Pool",
+      notification: "none",
+      support: "supported",
+    },
+    {
+      topic: "recruitment process",
+      title: "interview was unprofessional",
+      message: "interviewer was drunk",
+      owner: "Annie",
+      avatar: "./assets/images/doctorwho01.png",
+      status: "active",
+      timestamp: "2022-03-12T07:36:17+0000",
+      company: "UNKNOWN",
+      notification: "none",
+      support: "unsupported",
+    },
+    {
+      topic: "performance review",
+      title: "reviewer was racist",
+      message: "reviewer was racist",
+      owner: "Annie",
+      avatar: "./assets/images/blank-avatar.jpg",
+      status: "active",
+      timestamp: "2022-05-21T07:36:17+0000",
+      company: "",
+      notification: "none",
+      support: "unsupported",
+    },
+  ];
+
   return (
     <div className="blog-posts">
       <h2>BlogPosts</h2>
-      <BlogPost />
-      <BlogPost />
-      <BlogPost />
+      {blogPostsCollection.map((posts, _index) => (
+        <BlogPost id={_index} post={posts} />
+      ))}
     </div>
   );
 };
--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -2,10 +2,40 @@ import blankAvatar from "../assets/images/blank-avatar.jpg";
 
 const BlogPost = ({ post }) => {
   return (
-    <div className="blog-post">
-      <h2>blog post</h2>
+    <div className="container blog-post">
+      <div className="row">
+        <div className="col-1 post-image">
+          <div className="avatar-container">
+            <div className="image-container">
+              <img src={post.avatar ? post.avatar : blankAvatar} />
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <h5>{post.title}</h5>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-1 post-owner">{post.owner}</div>
+        <div className="col post-topic">{post.topic}</div>
+        <div className="col post-timestamp">{post.timestamp}</div>
+      </div>
+      <div className="row">
+        <div className="col post-message">
+          {post.message.substring(0, 150) + "..."}
+        </div>
+      </div>
+      <div className="row">
+        <div className="col post-support">{post.support}</div>
+        <div className="col post-notification">{post.notification}</div>
+        <div className="col post-reply">
+          <i class="bi bi-reply"></i>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default BlogPost;
+
+
