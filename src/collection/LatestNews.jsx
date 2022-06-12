c2a6364eefa21bacc8fc66245f567400cbc529a5
Owsama Hajjar <hajjar.owsama@outlook.com>
2022-06-12 11:56:15 +0100

    added news articles collection

--- src/collection/LatestNews.jsx
+++ src/collection/LatestNews.jsx
@@ -1,12 +1,39 @@
 import Article from "../components/Article";
 
 const LatestNews = () => {
+  const newsArticlesCollection = [
+    {
+      title: "review was focused on finding mistakes",
+      description: "they kept trying to find mistakes in everything I did",
+      publisher: "France24",
+      image: "https://source.unsplash.com/random/75x75?sig=1",
+      timestamp: "2022-02-11T07:36:17+0000",
+      link: "/#",
+    },
+    {
+      title: "finding mistakes",
+      description: " in everything I did",
+      publisher: "BBC News",
+      image: "https://source.unsplash.com/random/75x75?sig=2",
+      timestamp: "2022-02-15T07:36:17+0000",
+      link: "/#",
+    },
+    {
+      title: "focused on mistakes",
+      description: "they kept  I did",
+      publisher: "ITV",
+      image: "https://source.unsplash.com/random/75x75?sig=3",
+      timestamp: "2022-03-11T07:36:17+0000",
+      link: "/#",
+    },
+  ];
+
   return (
     <div className="latest-news">
       <h2>Latest News</h2>
-      <Article />
-      <Article />
-      <Article />
+      {newsArticlesCollection.map((article, _index) => (
+        <Article id={_index} article={article} />
+      ))}
     </div>
   );
 };
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,9 +1,37 @@
-const Article = () => {
+import { Link } from "react-router-dom";
+const blankImage = "https://source.unsplash.com/random/75x75?sig=1";
+
+const Article = ({ article }) => {
   return (
-    <div className="article">
-      <h2>Article</h2>
+    <div className="container article news-article">
+      <Link to={article.link} id="link">
+        <div className="container">
+          <div className="row">{article.title}</div>
+          <div className="row">
+            <div className="col-2">
+              <img src={article.image ? article.image : blankImage} />
+            </div>
+            <div className="col">
+              <div className="row">{article.description}</div>
+              <div className="row">
+                <div className="col">{article.publisher}</div>
+                <div className="col">{article.timestamp}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
 
 export default Article;
+
+// {
+//   title: "focused on mistakes",
+//   description: "they kept  I did",
+//   publisher: "ITV",
+//   image: "./assets/images/owsama-hat.jpg",
+//   timestamp: "2022-03-11T07:36:17+0000",
+//   link: "/#",
+// },
