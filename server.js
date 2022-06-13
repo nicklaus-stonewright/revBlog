2c1dd7e48679f79caf36b846e56048b5b7b95954
owsamahajjar <hajjar.owsama@outlook.com>
2022-06-13 21:46:08 +0100

    added basic server and db post handler

--- server.js
+++ server.js
@@ -1,5 +1,37 @@
-const PORT = 8000
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const axios = require('axios')
+const PORT = 8000;
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const axios = require('axios');
+const app = express();
+
+const token = "AstraCS:wXNSJHxOkfJYhxzsZsxuOEwY:19b1a75a425f9b594242867d83da4fb2c448d27264f50b3209ec6b7346f652ba";
+const url = "https://74709012-d3cd-49d5-9f26-1ae491ae2ca2-europe-west1.apps.astra.datastax.com/api/rest/v2/namespaces/posts/collections/blogposts";
+
+app.post('/blogposts', async (req, res) => {
+    const formData = req.body.formData
+
+    const options = {
+        method: 'PORT',
+        headers: {
+            Accepts: 'application/json',
+            'X-Cassandra-Token': token,
+            'Content-Type': 'application/json'
+        },
+        data: formData
+    }
+
+    try {
+        const response = await axios(url, options)
+        res.status(200).json(response.data)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ message: err })
+    }
+
+
+})
+
+
+app.listen(PORT, () => console.log(`server running on ${PORT}`));
+
--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,34 +1,36 @@
-import blankAvatar from "../assets/images/blank-avatar.jpg";
+import blankAvatar from "../assets/images/blankAvatar.jpg";
 
 const BlogPost = ({ post }) => {
   return (
     <div className="container blog-post">
       <div className="row">
         <div className="col-1 post-image">
-          <div className="avatar-container">
+          <div className="avatar-container post-element">
             <div className="image-container">
               <img src={post.avatar ? post.avatar : blankAvatar} />
             </div>
           </div>
         </div>
-        <div className="col">
+        <div className="col post-element">
           <h5>{post.title}</h5>
         </div>
       </div>
       <div className="row">
-        <div className="col-1 post-owner">{post.owner}</div>
-        <div className="col post-topic">{post.topic}</div>
-        <div className="col post-timestamp">{post.timestamp}</div>
+        <div className="col-1 post-owner post-element">{post.owner}</div>
+        <div className="col post-topic post-element">{post.topic}</div>
+        <div className="col post-timestamp post-element">{post.timestamp}</div>
       </div>
       <div className="row">
-        <div className="col post-message">
+        <div className="col post-message post-element">
           {post.message.substring(0, 150) + "..."}
         </div>
       </div>
-      <div className="row">
-        <div className="col post-support">{post.support}</div>
-        <div className="col post-notification">{post.notification}</div>
-        <div className="col post-reply">
+      <div className="row interaction-row">
+        <div className="col post-support post-element">{post.support}</div>
+        <div className="col post-notification post-element">
+          {post.notification}
+        </div>
+        <div className="col post-reply post-element">
           <i class="bi bi-reply"></i>
         </div>
       </div>
@@ -37,5 +39,3 @@ const BlogPost = ({ post }) => {
 };
 
 export default BlogPost;
-
-
